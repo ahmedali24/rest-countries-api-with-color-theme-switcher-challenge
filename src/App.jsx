41e3e245a92e54da59c0bf2243d366625595ca9a
ahmed97 <sirahmed97@gmail.com>
2022-04-18 01:28:49 -0600

    starting on darkmode + readme update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,63 @@
-import React from "react";
-import Home from "./pages/Home";
+import React, { useState, useEffect, useContext, createContext } from "react";
 import "./App.css";
+import Header from "./components/Header/Header";
+import Search from "./components/Search/Search";
+import Countries from "./components/Countries/Countries";
+
+export const ThemeContext = createContext("light");
 
 const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
+  const [theme, setTheme] = useState("dark");
+
+  const getCountries = async () => {
+    const response = await fetch("https://restcountries.com/v2/all");
+    const countries = await response.json();
+    setCountries(countries);
+  };
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className="app">
-      <Home />
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="app" id={theme}>
+        <Header />
+        <Search
+          handleChange={handleChange}
+          setCountries={setCountries}
+          countries={countries}
+        />
+        <div className="countries">
+          {filteredCountries.map((country) => {
+            return (
+              <Countries
+                key={country.numericCode}
+                flag={country.flag}
+                name={country.name}
+                population={country.population}
+                region={country.region}
+                capital={country.capital}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
